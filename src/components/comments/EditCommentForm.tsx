520ec00fd24bc62192d7be7d6cfa666f80f9e754
Sam <samueljoh329@gmail.com>
2022-10-06 17:11:07 -0500

    removed dark mode on edit comment form

--- src/components/comments/EditCommentForm.tsx
+++ src/components/comments/EditCommentForm.tsx
@@ -39,14 +39,14 @@ export const EditCommentForm = ({
       <form onSubmit={onSubmit} className="flex-1">
         <label
           htmlFor="comment"
-          className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+          className="sr-only mb-2 text-sm font-medium text-gray-900"
         >
           Update Comment
         </label>
         <textarea
           id="comment"
           rows={4}
-          className="mb-2 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-rose-500 focus:ring-rose-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-rose-500 dark:focus:ring-rose-500"
+          className="mb-2 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-rose-500 focus:ring-rose-500"
           placeholder="Leave a comment..."
           value={comment}
           onChange={(e) => setComment(e.target.value)}
@@ -55,7 +55,7 @@ export const EditCommentForm = ({
         <FadeIn show={comment !== initialComment.content}>
           <button
             type="submit"
-            className="mr-2 rounded-lg bg-rose-300 px-4 py-2 text-center text-sm font-medium text-black hover:bg-rose-400 focus:outline-none focus:ring-4 focus:ring-rose-100 dark:bg-rose-400 dark:hover:bg-rose-500 dark:focus:ring-rose-900"
+            className="mr-2 rounded-lg bg-rose-300 px-4 py-2 text-center text-sm font-medium text-black hover:bg-rose-400 focus:outline-none focus:ring-4 focus:ring-rose-100"
           >
             Update
           </button>
@@ -68,7 +68,7 @@ export const EditCommentForm = ({
               onClose?.();
               setComment("");
             }}
-            className="rounded-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-rose-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+            className="rounded-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-rose-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200"
           >
             Cancel
           </button>
